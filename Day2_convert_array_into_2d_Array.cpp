46084a038ee977d686fc5481c75794209c5e434b
yashwant938 <ranayash2022@gmail.com>
2024-01-02 19:42:18 +0530

    brute force

---
+++ Day2_convert_array_into_2d_Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+     vector<vector<int>> ans;
+     unordered_map<int,int>freq;
+     for(auto el:nums){
+         freq[el]++;
+         if(ans.size()<freq[el])
+            ans.push_back({});
+        ans[freq[el]-1].push_back(el);
+     }   
+     return ans;
+    }
+};
